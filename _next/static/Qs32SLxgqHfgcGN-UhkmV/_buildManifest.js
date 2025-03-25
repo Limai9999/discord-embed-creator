7666424e92a559bad347b5c6305410eafe111157
Alexander Nafasov <sasha.nafasov@gmail.com>
2025-03-25 17:20:04 +0500

    Updates

--- _next/static/Qs32SLxgqHfgcGN-UhkmV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/815-553936b507597728.js","static/chunks/pages/index-27ca435de97a26a8.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
